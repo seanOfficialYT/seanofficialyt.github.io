d062a3250c6892b641be9d385fbf7633249f6925
seanOfficialYT <82135361+seanOfficialYT@users.noreply.github.com>
2021-04-10 02:05:23 +0200

    seanOfficialYT website V1.3

---
+++ javascript/JavaScript.js
@@ -0,0 +1,15 @@
+﻿/*var FrontpageVid = document.getElementById(fpvideo)
+
+function playPause(FrontpageVid) {
+    if (FrontpageVid.pause)
+        FrontpageVid.play();
+    else
+        FrontpageVid.pause();
+}
+
+function muteUnmute(FrontpageVid) {
+    if (FrontpageVid.muted)
+        FrontpageVid.unmute();
+    else
+        FrontpageVid.mute();
+}*/
